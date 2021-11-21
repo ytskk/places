85c0da91b2d6d2cb8cc3c8de3a7604dc629e1cfc
ytskk <thefrozenewr@gmail.com>
2021-11-21 22:43:11 +0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screens/sight_screen.dart';
 
 void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          body: MyFirstWidget(),
-        ),
-        title: 'Title');
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,104 +11,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.indigoAccent,
       ),
-      home: MySecondWidget(),
+      home: SightListScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: Icon(Icons.add),
-        ));
-  }
-}
-
-class MySecondWidget extends StatefulWidget {
-  const MySecondWidget({Key? key}) : super(key: key);
-
-  @override
-  _MySecondWidgetState createState() => _MySecondWidgetState();
-}
-
-class _MySecondWidgetState extends State<MySecondWidget> {
-  var _rerendiringCount = 0;
-
-  var _rerendedingCount2 = 0;
-
-  getContextRuntime() => print(context.runtimeType);
-
-  @override
-  void didUpdateWidget(covariant MySecondWidget oldWidget) {
-    _rerendedingCount2 += 1;
-    print(_rerendedingCount2);
-    super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _rerendiringCount += 1;
-    print('statful rerenderings: $_rerendiringCount');
-    return Container(
-        child: Center(
-            child: Text(
-      'Hello!',
-      style: TextStyle(fontFamily: 'Roboto', fontWeight: FontWeight.w300),
-    )));
-  }
-}
-
-class MyFirstWidget extends StatelessWidget {
-  MyFirstWidget({Key? key}) : super(key: key);
-
-  var _rerendiringCount = 0;
-
-  // getContextRuntime() => print(context.runtimeType);
-
-  @override
-  Widget build(BuildContext context) {
-    _rerendiringCount += 1;
-    print('rerendeding count: $_rerendiringCount');
-    return Container(child: Center(child: Text('Hel2lo!')));
   }
 }
---
+++ lib/ui/screens/sight_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SightListScreen extends StatefulWidget {
+  const SightListScreen({Key? key}) : super(key: key);
+
+  @override
+  _SightListScreenState createState() => _SightListScreenState();
+}
+
+class _SightListScreenState extends State<SightListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [Text('Hello'), TextField()],
+        ));
+  }
+}
